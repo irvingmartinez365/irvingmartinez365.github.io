2883a2bec1727b334bdbbae5f6ced9ccf6c1e661
Irving Mendoza <irvingamer4114@gmail.com>
2021-12-13 21:04:57 -0600

    uso de templates con JS para contenido de articulos.html

---
+++ js/articles.js
@@ -0,0 +1,32 @@
+const articlesContainer = document.getElementById('articles-contianer');
+const template = document.getElementById('articulo-template').content;
+
+const url = 'http://localhost:5500/js/json/articles-info.json';
+
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', url);
+xhr.responseType = 'json'
+xhr.addEventListener('load', () => {
+    const response = xhr.response;
+    
+
+    const frag = document.createDocumentFragment();
+
+    for(const info of response){
+        /* let link = `lectura.html?articleID=${info["id"]}` */
+        template.querySelector('#article-template__name').textContent = info["name"];
+        template.querySelector('.article-container #article-template__txt').textContent = info["description"];
+        template.querySelector('.article-container #article-template__topic span').textContent = info["topic"];
+
+        const element = template.cloneNode(true);
+
+        console.log(element);
+        frag.appendChild(element);
+    }
+
+    articlesContainer.appendChild(frag);
+
+}) 
+
+xhr.send()
